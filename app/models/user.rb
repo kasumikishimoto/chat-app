4f5411cfb408b32b0fdaacd622876db29f8f0759
kasumikishimoto <kasumikishimot00@gmail.com>
2021-09-08 12:46:02 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name,presence: true 
   #nameカラムは、空の時DBにデータを保存しないバリデーション！
+  has_many :room_users
+  has_many : rooms, though: :room_users
 end
